a856b446ab9a1de952acc9505519fb20d5b87dc4
nearbyx000 <misterpipka881@gmail.com>
2026-02-03 17:45:57 +0300

    architecture reworked

--- razbor-3.py
+++
@@ -1,6 +0,0 @@
-from itertools import *
-schet = 0
-for i in product('12345678',repeat=5):
-    if i.count("2") == 1:
-        schet += 1
-print(schet)
--- task-8.py
+++
@@ -1,8 +0,0 @@
-from itertools import *
-numbers = 1
-schet = 0
-for i in product(sorted("ИНФОРМАТ"),repeat=5):
-    if numbers % 2 != 0 and i[0] != "О" and 1 <= i.count("Н") <=2:
-        schet += 1
-    numbers += 1
-print(schet)
